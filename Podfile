55ae60163d36fae1c35b4ff1d5cff0cab40a9ff0
yogoken <yogo.k@husky.neu.edu>
2017-08-05 00:51:35 +0900

    Install cocoapod

---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'Twitter' do
+  pod 'NCMB', :git => 'https://github.com/NIFTYCloud-mbaas/ncmb_ios.git'
+end
--- Twitter/AppDelegate.swift
+++ Twitter/AppDelegate.swift
@@ -7,40 +7,30 @@
 //
 
 import UIKit
+import NCMB
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
-
+    //********** APIキーの設定 **********
+    let applicationkey = "1e677f9fe6ed2acfa53e900f0ab639c289bb981734fbf5af68f93432836ceda4"
+    let clientkey      = "523bf0891261378a1e825d8a6ee3c7a1731ba351b84c34c010774751fcc9c89d"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //********** SDKの初期化 **********
+        NCMB.setApplicationKey(applicationkey, clientKey: clientkey)
+        //▼▼▼起動時に処理される▼▼▼
+        // データストアにデータを保存
+        let obj = NCMBObject(className: "TestClass")
+        obj?.setObject("Hello, NCMB!", forKey: "message")
+        obj?.saveInBackground({ (error) in
+            if error != nil {
+                // 保存に失敗した場合の処理
+            } else {
+                // 保存に成功した場合の処理
+            }
+        })
+        //▲▲▲起動時に処理される▲▲▲
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
-
